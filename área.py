d112255fd5fd2ad64f61395e403f2973513d2a44
jot4hpe <jot4hpe.com>
2024-05-06 13:58:21 -0300

    commit calc area

---
+++ área.py
@@ -0,0 +1,5 @@
+n1 = float(input('qual a largura da parede?: '))
+n2 = float(input('qual a altura da parede?: '))
+a = n1 * n2
+tinta = a/2
+print(f'coma a area de {a}, necessita de {tinta} litros de tinta para pintar a parede')
